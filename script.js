5749a221c7c8ad28841b25695469bbb9d3db3a73
MatheusVicente11 <matheusvicente8194k@gmail.com>
2024-09-06 12:14:00 -0300

    aula 05-09

---
+++ script.js
@@ -0,0 +1,8 @@
+var numero = parseInt(prompt("Digite um número inteiro: "));
+if (numero > 0){
+    console.log("Este número é positivo!");
+}else if(numero < 0){
+    console.log("Este número é negativo!");
+}else if(numero == 0){
+    console.log("O número digitado é zero!");
+}
